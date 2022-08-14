34eb25bcad3fd86ee89ad085ad890c4ad77e8f1b
George Moore <georgemoore@Georges-MacBook-Air.local>
2022-08-14 20:00:07 +0100

    update styling of LoadingModal and NicknameModal components

--- user-interface/src/components/LoadingModal/LoadingModal.js
+++ user-interface/src/components/LoadingModal/LoadingModal.js
@@ -1,20 +1,21 @@
 
-import React from "react";
-import ReactDOM from "react-dom";
 import styles from "./LoadingModal.module.css";
+import ReactDOM from "react-dom";
+import { useSelector } from "react-redux"; 
 
-export default class LoadingModal extends React.Component {
-	render() {
-		if (this.props.open == false) return null;
-		return ReactDOM.createPortal(
-			<div className={styles.window}>
-				<div className={styles.modal}>
-					<div className={styles.container}>
-						<div className={styles.circle}></div>
-						<h1>Connecting...</h1>
-					</div>
+export default function LoadingModal(props) {
+
+	const color = useSelector(state => state.color);
+	if (props.open == false) return null;
+
+	return ReactDOM.createPortal(
+		<div className={styles.window} style={{ backgroundColor: color.quaternaryColor }}>
+			<div className={styles.modal}>
+				<div className={styles.container}>
+					<div style={{ borderBottomColor: color.secondaryColor, borderTopColor: color.secondaryColor }} className={styles.circle}></div>
+					<h1 style={{ color: color.secondaryColor }}>Connecting...</h1>
 				</div>
-			</div>, document.getElementById("portal")
-		)
-	}
+			</div>
+		</div>, document.getElementById("portal")
+	)
 }
--- user-interface/src/components/NicknameModal/NicknameModal.js
+++ user-interface/src/components/NicknameModal/NicknameModal.js
@@ -2,10 +2,12 @@
 import ReactDOM from "react-dom";
 import styles from "./NicknameModal.module.css";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function NicknameModal(props) {
 
 	const [nickname, setNickname] = useState("");
+	const color = useSelector(state => state.color);
 
 	const onInputChange = (event) => {
 		setNickname(event.target.value);
@@ -14,14 +16,14 @@ export default function NicknameModal(props) {
 	if (props.open == false) { return null };
 	return ReactDOM.createPortal(
 		<div className={styles.window}>
-			<div className={styles.modal}>
-				<h1>Join Lab</h1>
+			<div className={styles.modal} style={{ color: color.tertiaryColor }}>
+				<h1 style={{ color: color.secondaryColor }}>Join Lab</h1>
 				<span></span>
-				<h2>Enter a display name:</h2>
-				<input value={nickname} placeholder="John Doe" type="text" onChange={onInputChange}/>
+				<h2 style={{ color: color.secondaryColor }}>Enter a display name:</h2>
+				<input value={nickname} placeholder="John Doe" type="text" onChange={onInputChange} style={{ color: color.secondaryColor }}/>
 				<div className={styles.container}>
-					<button className={styles.cancelButton} onClick={() => window.location.href = "http://localhost:5000"}>Cancel</button>
-					<button className={styles.joinButton} onClick={() => props.onSubmit(nickname)}>Join</button>
+					<button style={{ backgroundColor: color.secondaryColor, color: color.quaternaryColor }} className={styles.cancelButton} onClick={() => window.location.href = "http://localhost:5000"}>Cancel</button>
+					<button style={{ backgroundColor: color.primaryColor, color: color.quaternaryColor }} className={styles.joinButton} onClick={() => props.onSubmit(nickname)}>Join</button>
 				</div>
 			</div>
 
