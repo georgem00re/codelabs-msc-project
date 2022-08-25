d1b10036b8ef14437436cc29bab23eaa189422df
George Moore <georgemoore@Georges-MacBook-Air.local>
2022-08-25 14:19:21 +0100

    install and implement cors in express microservice

--- express-server/index.js
+++ express-server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
 const { createRoomController } = require("./controllers/createRoomController.js")
 const { rootController } = require("./controllers/rootController.js")
 const { roomController } = require("./controllers/roomController.js")
 
+app.use(cors());
 app.use(express.static("public"));
 
 app.get("/", rootController);
