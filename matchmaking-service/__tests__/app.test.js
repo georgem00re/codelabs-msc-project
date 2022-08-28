93cbc64d735469bfbfd9be5cef7d14a7e7c17357
George Moore <georgemoore@Georges-MacBook-Air.local>
2022-08-28 12:44:20 +0100

    install Jest and add __tests__ directory to socketio-server microservice

--- matchmaking-service/__tests__/app.test.js
+++ matchmaking-service/__tests__/app.test.js
@@ -4,14 +4,19 @@ const request = require("supertest");
 
 describe("POST /", () => {
 
-	test("Status code is 200", async () => {
+	test("status code", async () => {
 		const res = await request(app).post("/").send({ labID: 12345})
 		expect(res.statusCode).toEqual(200);
 	})
 
-	test("Cors", async () => {
+	test("access-control-allow-origin", async () => {
 		const res = await request(app).post("/").send({ labID: 12345})
 		expect(res.headers["access-control-allow-origin"]).toEqual("*");
+	})
+
+	test("res.body", async () => {
+		const res = await request(app).post("/").send({ labID: 12345})
+		expect(typeof parseInt(res.body.port)).toBe("number");
 	})
 
 })
