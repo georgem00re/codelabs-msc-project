8c5be195f7174c4c3f545f0ee772c109738415b8
George Moore <georgemoore@Georges-MacBook-Air.local>
2022-09-03 15:05:00 +0100

    change name of socketio-server to lab-server

--- matchmaking-service/utils/spawnContainer.js
+++ matchmaking-service/utils/spawnContainer.js
@@ -2,7 +2,7 @@
 const { docker } = require("./dockerode.js");
 
 async function spawnContainer(labID) {
-	const options = { Image: "codelabs-socketio-server", name: labID ,HostConfig: { PortBindings: { "7000/tcp": [{ HostPort: "0" }] }, NetworkMode: "bridge" }, ExposedPorts: { "7000/tcp": {} } }
+	const options = { Image: "codelabs-lab-server", name: labID ,HostConfig: { PortBindings: { "7000/tcp": [{ HostPort: "0" }] }, NetworkMode: "bridge" }, ExposedPorts: { "7000/tcp": {} } }
 	const container = await docker.createContainer(options);
 	const containerID = container.id;
 	await container.start();
